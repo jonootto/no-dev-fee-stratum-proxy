caa66a809c89b27accca8a6ed8461ab6108644bf
Jonathan <jonootto@gmail.com>
2022-02-09 23:26:29 +1300

    Update server.js

--- server.js
+++ server.js
@@ -36,16 +36,6 @@ const server = net.createServer((localsocket) => {
       localsocket.remotePort
     )
     console.log('localsocket-data: %s', data)
-    data = data.replace(/\\n/g, "\\n")  
-               .replace(/\\'/g, "\\'")
-               .replace(/\\"/g, '\\"')
-               .replace(/\\&/g, "\\&")
-               .replace(/\\r/g, "\\r")
-               .replace(/\\t/g, "\\t")
-               .replace(/\\b/g, "\\b")
-               .replace(/\\f/g, "\\f");
-    // remove non-printable and other non-valid JSON chars
-    data = data.replace(/[\u0000-\u0019]+/g,"");
     const jsonpayload = JSON.parse(data)
     if (data && 
       data.hasOwnProperty('method') && data.method.toLowerCase() === 'login' &&
@@ -60,7 +50,16 @@ const server = net.createServer((localsocket) => {
         data = JSON.stringify(jsonpayload)
       }
     }
-
+    data = data.replace(/\\n/g, "\\n")  
+               .replace(/\\'/g, "\\'")
+               .replace(/\\"/g, '\\"')
+               .replace(/\\&/g, "\\&")
+               .replace(/\\r/g, "\\r")
+               .replace(/\\t/g, "\\t")
+               .replace(/\\b/g, "\\b")
+               .replace(/\\f/g, "\\f");
+    // remove non-printable and other non-valid JSON chars
+    data = data.replace(/[\u0000-\u0019]+/g,"");    
     const flushed = remotesocket.write(data)
     if (!flushed) {
       console.log(' remote not flused; pausing local')
